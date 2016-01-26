d715503d09a2cf6348e5af72e4cc1d5280401a8b
EmilyWebber <egwebber@gmail.com>
2016-01-25 21:16:15 -0600

    Add open data file

---
+++ main.py
@@ -0,0 +1,19 @@
+import utils
+import bs4
+import json
+import csv
+
+limiting_domain = 'council.nyc.gov'
+
+# first step: get everything from this year and the last
+
+# from this page http://legistar.council.nyc.gov/Legislation.aspx
+# don't need to read into the links for starters, just grab the issue names and title
+
+
+# crunch the hrefs
+
+
+# get key issues
+
+
